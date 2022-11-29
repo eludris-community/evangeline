6da9cfb532e197e1eb15be50422bb529fd9ac637
Sham <thesham@protonmail.com>
2022-11-29 23:10:52 +0200

    chore: add typings file

---
+++ src/lib/types/index.d.ts
@@ -0,0 +1,33 @@
+export declare function Evangeline(name: string, options?: BotOptions): Bot
+
+export interface BotOptions {
+    gatewayURL?: string;
+    restURL?: string;
+}
+
+export interface MessagePayload {
+    author: string;
+    content: string;
+}
+
+export interface MessageResponse {
+    author: string;
+    content: string;
+}
+
+export class Message {
+    public author: string;
+    public content: string;
+
+    constructor(author: string, content: string);
+}
+
+export class Bot {
+    public name: string;
+    private options?: BotOptions;
+    public ws: WebSocket;
+    public rest: string;
+    constructor(name: string, options?: BotOptions);
+}
+
+export type MessageType = MessagePayload | MessageResponse;
