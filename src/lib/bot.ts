00189144b86f088daa3be290bfa29116584c6f7e
Sham <thesham@protonmail.com>
2022-11-30 17:51:03 +0200

    docs(Bot): add documentation for vars and params

--- src/lib/bot.ts
+++ src/lib/bot.ts
@@ -5,12 +5,38 @@ import { Message, MessageResponse } from './types/message'
 const DEFAULT_REST_URL = 'https://eludris.tooty.xyz/'
 const DEFAULT_WS_URL = 'wss://eludris.tooty.xyz/ws/'
 
+/**
+ * The options for the bot.
+ */
 interface BotOptions {
+    /**
+     * The gateway URL to connect to.
+     * @public
+     */
     gatewayURL?: string;
+    /**
+     * The REST URL to send requests to.
+     */
     restURL?: string;
 }
 
 export declare interface Bot {
+    /**
+     * A function to listen to certain events.
+     * @param event The event to listen to.
+     * @param listener The parameters (if needed) to be used in the event.
+     * @public
+     * @example
+     * import { Bot } from 'evangeline';
+     * 
+     * const bot = new Bot('evangeline bot')
+     * 
+     * bot.on('messageCreate', async (message) => {
+     *     // event code here
+     * })
+     * 
+     * bot.connect()
+     */
     on(event: 'messageCreate', listener: (message: Message) => void): this;
     on(event: 'ready', listener: () => void): this;
     on(event: 'error', listener: (error: Error) => void): this;
@@ -21,12 +47,21 @@ export class Bot extends EventEmitter {
     private options?: BotOptions
     public ws: WebSocket | null = null
     public rest: string
-    private interval: NodeJS.Timer | null = null;
+    private interval: NodeJS.Timer | null = null
 
     /**
      * The main bot class.
      * @param name The desired name of the bot.
      * @param options The options for the bot.
+     * @public
+     * @example
+     * import { Bot } from 'evangeline';
+     * 
+     * const bot = new Bot(
+     *      'evangeline test', {
+     *          gatewayURL: 'wss://some.ws-url.xyz/ws/', // optional
+     *          restURL: 'https://some.rest-url.xyz/' // optional
+     * })
      */
     constructor(name: string, options?: BotOptions) {
         super()
@@ -37,10 +72,17 @@ export class Bot extends EventEmitter {
     
     /**
      * Connects to the Eludris gateway.
+     * @public
+     * @example
+     * import { Bot } from 'evangeline';
+     * 
+     * const bot = new Bot('evangeline test')
+     * 
+     * bot.connect()
      */
     connect() {
         this.ws = new WebSocket(this.options?.gatewayURL || DEFAULT_WS_URL)
-        console.log(this.ws.readyState);
+        console.log(this.ws.readyState)
         
         this.ws.on('open', () => {
             this.emit('ready')
@@ -66,6 +108,7 @@ export class Bot extends EventEmitter {
 
     /**
      * Closes the connection to the Eludris gateway.
+     * @public
      */
     close() {
         if (this.ws?.readyState === WebSocket.OPEN) {
@@ -77,6 +120,7 @@ export class Bot extends EventEmitter {
     /**
      * A private function to create messages.
      * @param message The message to create
+     * @private
      */
     private async createMessage(message: Message): Promise<MessageResponse> {
         const response = await fetch(`${this.rest}/messages`, {
@@ -92,13 +136,23 @@ export class Bot extends EventEmitter {
     /**
      * Send a message to Eludris.
      * @param content The content of the message.
+     * @public
+     * @example
+     * import { Bot } from 'evangeline';
+     * 
+     * const bot = new Bot('evangeline test')
+     * 
+     * bot.on('ready', async () => {
+     *     await bot.sendMessage('woah, I'm alive!')
+     * })
+     * @returns {@link Promise<MessageResponse>}
      */
     async sendMessage(content: string): Promise<MessageResponse> {
         return await this.createMessage(new Message(this.name, content))
     }
 
     /**
-     * Send a message to Eludris. (Alias for sendMessage)
+     * Send a message to Eludris. (Alias for {@link sendMessage})
      * @param content The content of the message.
      */
     async send(content: string): Promise<MessageResponse> {
