0d2675a7deab5c3c3dcc65247532d0507dc51a1b
Sham <thesham@protonmail.com>
2022-11-30 17:53:32 +0200

    docs(Message): add docs for classes & interfaces

--- src/lib/types/message.ts
+++ src/lib/types/message.ts
@@ -1,15 +1,45 @@
+/**
+ * Returns the payload of a posted message.
+ */
 export interface MessagePayload {
+    /**
+     * The author of the message.
+     * @readonly
+     */
     author: string;
+    /**
+     * The content of the message.
+     * @readonly
+     */
     content: string;
 }
 
+/**
+ * Returns the response of a message. Similar to {@link MessagePayload}.
+ */
 export interface MessageResponse {
+    /**
+     * The author of the message.
+     * @readonly
+     */
     author: string;
+    /**
+     * The content of the message.
+     * @readonly
+     */
     content: string;
 }
 
 export class Message {
+    /**
+     * The author of the message.
+     * @public
+     */
     public author: string
+    /**
+     * The content of the message.
+     * @public
+     */
     public content: string
 
     /**
